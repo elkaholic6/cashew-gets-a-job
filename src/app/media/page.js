5d5c7f61d5875c96cce5d7caab9523e410cf8e55
elkaholic6 <wyattchamberlin6@gmail.com>
2024-10-11 10:40:26 -0400

    optimized Images for nextjs

--- src/app/media/page.js
+++ src/app/media/page.js
@@ -89,8 +89,8 @@ function page() {
                                         <div className="relative flex justify-center w-full max-xs:h-wallpaper-smscreen xs:h-wallpaper-mdscreen md:h-wallpaper-lgscreen">
                                             <Image src="/wallpaper_1.png" alt="wallpaper" fill={true} sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw" placeholder="empty"/>
                                         </div>
-                                        <div className="relative flex justify-center w-full max-xs:h-wallpaper-smscreen xs:h-wallpaper-mdscreen md:h-wallpaper-lgscreen" >
-                                            <Image src="/wallpaper_2.png" alt="wallpaper2" fill={true} sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw" placeholder="empty" />
+                                        <div className="relative flex justify-center w-full h-full" >
+                                            <Image src="/wallpaper_2.png" alt="wallpaper2" width={337} height={600} placeholder="empty" />
                                         </div>
                                         <div className="relative flex justify-center w-full max-xs:h-wallpaper-smscreen xs:h-wallpaper-mdscreen md:h-wallpaper-lgscreen">
                                             <Image src="/wallpaper_3.png" alt="wallpaper3" fill={true} sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw" placeholder="empty"/>
