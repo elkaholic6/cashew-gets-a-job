925ec71854c951b7308843fb882054154752fe3e
elkaholic6 <wyattchamberlin6@gmail.com>
2024-10-11 12:20:06 -0400

    optimized Images for nextjs

--- src/app/components/BookPages.js
+++ src/app/components/BookPages.js
@@ -97,7 +97,8 @@ function BookPages() {
             <div className="flex justify-center">
               <div className="absolute w-[95%] md:w-3/4 max-w-[2048px] h-fit my-4 sm:mt-4 xl:mt-6 2xl:mt-8 bg-cyan-300 z-30 border-none sm:border-[8px] md:border-[16px] border-cyan-300 rounded-xl overflow-hidden shadow-[0_0px_25px_-3px_rgba(0,0,0,0.4)]">
                   <div className="relative w-full h-full flex flex-wrap max-md:h-smbook-pages-screen md:h-mdbook-pages-screen" ref={boxesRef}>
-                  <Image className="object-cover z-0 invisible" src='/rockstar.png' alt="Rockstar" fill={true} sizes="50vw" quality={0}/>
+                    {/* 3840 x 2400 is intrinsic ratio */}
+                  <Image className="object-cover z-0 invisible" src='/rockstar.png' alt="Rockstar" width={2048} height={1280} sizes="(max-width: 768px) 95vw, 75vw" quality={0}/> 
                   <div className="absolute flex w-full h-full leading-none z-[90] border-2 border-gray-700 rounded-xl overflow-hidden">
                           {layout.items.map((item) => (
                           <div
@@ -105,9 +106,9 @@ function BookPages() {
                               key={item.id}
                               className="box absolute"
                           >
-                            <div className="relative flex justify-center w-[95vw] md:w-[75vw] max-md:h-smbook-pages-screen md:h-mdbook-pages-screen">
-                              <Image className="object-cover rounded-xl overflow-hidden" src={item.image} alt={`NFT ${item.id}`} fill={true} sizes="50vw" />
-                            </div>
+                            {/* <div className="relative flex justify-center w-[95vw] md:w-[75vw] max-md:h-smbook-pages-screen md:h-mdbook-pages-screen"> */}
+                              <Image className="object-cover rounded-xl overflow-hidden" src={item.image} alt={`NFT ${item.id}`} width={3840} height={2048} sizes="(max-width: 768px) 95vw, 75vw"/>
+                            {/* </div> */}
                           </div>
                           ))}
                   </div>
