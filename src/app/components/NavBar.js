60658915772c941af5b2c4b9e95d86702966c2ef
elkaholic6 <wyattchamberlin6@gmail.com>
2024-10-12 11:38:30 -0400

    adjusted dropdown menu for short phone screens

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -263,7 +263,7 @@ function NavBar() {
         <div className="lg:hidden">
           <div ref={backdropRef} className={`backdropNav w-screen fixed inset-y-0 right-0 h-screen bg-black opacity-50 ${mobileMenuOpen ? 'block' : 'hidden'}`}/>
             <div ref={mobileMenuRef} className="fixed inset-y-0 right-0 z-50 w-full h-fit overflow-y-auto" style={{ transform: 'translateY(-100%)' }}>
-              <div className="bg-[#836EE7] rounded-b-xl border-b-[6px] border-cyan-200 h-fit py-6 px-6">
+              <div className="bg-[#836EE7] rounded-b-xl border-b-[6px] border-cyan-200 h-fit py-6 px-6 overflow-y-auto">
                 <div className="flex items-start justify-between">
                   <div className="w-full h-fit">
                     <a href="#home" className="flex justify-center -mx-1.5 px-1.5">
